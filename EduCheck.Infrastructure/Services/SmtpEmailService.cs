8d64c80367f33c88f49500bf9f9fa12b6eafa8f0
KaBaN4iK357 <anatoliikljukin@mail.ru>
2026-04-09 16:48:06 +0400

    Create SmtpEmailService.cs

---
+++ EduCheck.Infrastructure/Services/SmtpEmailService.cs
@@ -0,0 +1,32 @@
+﻿using EduCheck.Core.Interfaces;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace EduCheck.Infrastructure.Services;
+
+public class SmtpEmailService : IEmailService
+{
+    private readonly IConfiguration _config;
+
+    public SmtpEmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SendFeedbackAsync(string toEmail, string subject, string body)
+    {
+        var settings = _config.GetSection("EmailSettings");
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("EduCheck Bot", settings["Email"]));
+        message.To.Add(new MailboxAddress("", toEmail));
+        message.Subject = subject;
+        message.Body = new TextPart("plain") { Text = body };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync("smtp.mail.ru", 465, true);
+        await client.AuthenticateAsync(settings["Email"], settings["Password"]);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
