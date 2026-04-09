2e43fd981c02cb3bc48f910c16a9cba3ea40b2c8
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-04-09 11:51:05 +0400

    Update Program.cs

--- EduCheck.EmailWorker/Program.cs
+++ EduCheck.EmailWorker/Program.cs
@@ -1,6 +1,25 @@
+using EduCheck.Core.Interfaces;
 using EduCheck.EmailWorker;
+using EduCheck.Infrastructure.Data;
+using EduCheck.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Minio;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var storageSettings = builder.Configuration.GetSection("StorageSettings");
+builder.Services.AddMinio(configureSource => configureSource
+    .WithEndpoint(storageSettings["Endpoint"])
+    .WithCredentials(storageSettings["AccessKey"], storageSettings["SecretKey"])
+    .WithSSL(false));
+
+builder.Services.AddScoped<IEmailParser, EmailParser>();
+builder.Services.AddScoped<IFileStorage, MinioFileStorage>();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
