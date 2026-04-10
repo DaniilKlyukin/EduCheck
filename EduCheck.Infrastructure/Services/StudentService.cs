a7c4304cc257e252e960b2ad2ac594ec4b9af68e
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-04-10 18:27:01 +0400

    Create StudentService.cs

---
+++ EduCheck.Infrastructure/Services/StudentService.cs
@@ -0,0 +1,23 @@
+﻿using EduCheck.Core.Entities;
+using EduCheck.Core.Interfaces;
+using EduCheck.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduCheck.Infrastructure.Services;
+
+public class StudentService(IDbContextFactory<AppDbContext> dbFactory) : IStudentService
+{
+    public async Task<List<Student>> GetAllStudentsAsync()
+    {
+        using var db = await dbFactory.CreateDbContextAsync();
+
+        return await db.Students.ToListAsync();
+    }
+
+    public async Task<Student?> GetStudentByIdAsync(Guid id)
+    {
+        using var db = await dbFactory.CreateDbContextAsync();
+
+        return await db.Students.FindAsync(id);
+    }
+}
