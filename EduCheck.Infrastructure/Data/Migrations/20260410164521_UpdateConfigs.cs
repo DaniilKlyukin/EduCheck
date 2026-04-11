26a1d1847b68ef1f216b6d7932e651a487202927
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-04-11 19:15:22 +0400

    Add migrations

---
+++ EduCheck.Infrastructure/Data/Migrations/20260410164521_UpdateConfigs.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduCheck.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConfigs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_Title",
+                table: "Subjects",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_SubmissionId",
+                table: "Reviews",
+                column: "SubmissionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Submissions_SubmissionId",
+                table: "Reviews",
+                column: "SubmissionId",
+                principalTable: "Submissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Submissions_SubmissionId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_Title",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_SubmissionId",
+                table: "Reviews");
+        }
+    }
+}
